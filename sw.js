b57328dffca911f1e054c9fe0ded1eeae73c858c
postai <dpostai@gmail.com>
2018-11-29 22:50:01 -0200

    Updates

--- sw.js
+++ sw.js
@@ -26,40 +26,40 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-684f2a4de7600eeaa2cb.js"
+    "url": "webpack-runtime-783eb0f52f743ade91c1.js"
   },
   {
-    "url": "app-68164e70223db34a933c.js"
+    "url": "app-66449f783f031993f8ce.js"
   },
   {
-    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-2ab347e8ce9c31b0f61b.js"
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-a5814bd9535675e5b054.js"
   },
   {
     "url": "index.html",
-    "revision": "7a445b12b27401761774423c80bfca84"
+    "revision": "5d89ca82156a2c29e971bbeca4b9ce07"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6a67d10b2d119d8aa867d2f0958b47c3"
+    "revision": "e8808298803c87365075615f57e3ecb5"
   },
   {
     "url": "0.bc5c1186177246dac5a5.css"
   },
   {
-    "url": "0-4a2edf4ff1fef22be45c.js"
+    "url": "0-fff40548d3473fd268b4.js"
   },
   {
-    "url": "component---src-pages-index-js-746f0d3bc92eeaab84b2.js"
+    "url": "component---src-pages-index-js-fd21691e1386eec8c7cf.js"
   },
   {
-    "url": "1-ebbe8c94703a7b77b259.js"
+    "url": "1-034cb72b4c76177c1461.js"
   },
   {
     "url": "static/d/173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E.json",
     "revision": "c2508676a2f33ea9f1f0bf472997f9a0"
   },
   {
-    "url": "component---src-pages-404-js-11c845d2864ff9e40935.js"
+    "url": "component---src-pages-404-js-4a82b2480720bb5a7047.js"
   },
   {
     "url": "static/d/164/path---404-html-516-62a-NZuapzHg3X9TaN1iIixfv1W23E.json",
